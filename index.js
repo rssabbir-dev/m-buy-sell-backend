be7d6669871f6a33ac0906453689b86e252452d9
rssabbir <sabbirhossainrahad@gmail.com>
2022-11-26 21:21:46 +0600

    improve logic

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ require('dotenv').config();
 const port = process.env.PORT || 5000;
 const jwt = require('jsonwebtoken');
 const app = express();
+const stripe = require('stripe')(process.env.STRIPE_SECRET);
 
 //Middleware
 app.use(cors());
@@ -49,6 +50,7 @@ const run = async () => {
 		const categoryCollection = database.collection('categories');
 		const orderCollection = database.collection('orders');
 		const blogCollection = database.collection('blogs');
+		const paymentCollection = database.collection('payments');
 
 		const verifySeller = async (req, res, next) => {
 			const decoded = req.decoded;
@@ -156,6 +158,62 @@ const run = async () => {
 				res.send({ accessToken: token });
 			});
 		});
+		app.patch('/report-product/:uid', verifyJWT, async (req, res) => {
+			const decoded = req.decoded;
+			const uid = req.params.uid;
+			const id = req.query.id;
+			const prevReport = parseInt(req.query.reportCount);
+			if (uid !== decoded.uid) {
+				return res
+					.status(403)
+					.send({ message: 'Access Forbidden', code: 403 });
+			}
+			const filter = { _id: ObjectId(id) };
+			const option = { upsert: true };
+			const updatedDoc = {
+				$set: {
+					reported: true,
+					reportCount: prevReport + 1,
+				},
+			};
+
+			const result = await productCollection.updateOne(
+				filter,
+				updatedDoc,
+				option
+			);
+			res.send(result);
+		});
+
+		app.patch(
+			'/report-product-safe/:uid',
+			verifyJWT,
+			verifyAdmin,
+			async (req, res) => {
+				const decoded = req.decoded;
+				const uid = req.params.uid;
+				const id = req.query.id;
+				if (uid !== decoded.uid) {
+					return res
+						.status(403)
+						.send({ message: 'Access Forbidden', code: 403 });
+				}
+				const filter = { _id: ObjectId(id) };
+				const option = { upsert: true };
+				const updatedDoc = {
+					$set: {
+						reported: false,
+					},
+				};
+
+				const result = await productCollection.updateOne(
+					filter,
+					updatedDoc,
+					option
+				);
+				res.send(result);
+			}
+		);
 
 		//save new user
 		app.post('/users', async (req, res) => {
@@ -165,6 +223,26 @@ const run = async () => {
 		});
 
 		//All Admin Operation
+		app.get(
+			'/reported-products/:uid',
+			verifyJWT,
+			verifyAdmin,
+			async (req, res) => {
+				const decoded = req.decoded;
+				const uid = req.params.uid;
+				if (uid !== decoded.uid) {
+					return res
+						.status(403)
+						.send({ message: 'Access Forbidden', code: 403 });
+				}
+				const query = { reported: true };
+				const reportedProducts = await productCollection
+					.find(query)
+					.sort({ reportCount: 1 })
+					.toArray();
+				res.send(reportedProducts);
+			}
+		);
 		app.get('/user/admin/:uid', async (req, res) => {
 			const uid = req.params.uid;
 			const query = { uid };
@@ -244,6 +322,24 @@ const run = async () => {
 				res.send(result);
 			}
 		);
+		app.delete(
+			'/report-product-delete/:uid',
+			verifyJWT,
+			verifyAdmin,
+			async (req, res) => {
+				const decoded = req.decoded;
+				const uid = req.params.uid;
+				const id = req.query.id;
+				if (uid !== decoded.uid) {
+					return res
+						.status(403)
+						.send({ message: 'Access Forbidden', code: 403 });
+				}
+				const filter = { _id: ObjectId(id) };
+				const result = await productCollection.deleteOne(filter);
+				res.send(result);
+			}
+		);
 
 		//All Seller Operation
 
@@ -267,6 +363,19 @@ const run = async () => {
 			const orders = await orderCollection.find(query).toArray();
 			res.send(orders);
 		});
+		app.get('/order/:uid', verifyJWT, async (req, res) => {
+			const decoded = req.decoded;
+			const uid = req.params.uid;
+			if (uid !== decoded.uid) {
+				return res
+					.status(403)
+					.send({ message: 'Access Forbidden', code: 403 });
+			}
+			const id = req.query.id;
+			const query = { _id: ObjectId(id) };
+			const order = await orderCollection.findOne(query);
+			res.send(order);
+		});
 		app.post('/orders/:uid', verifyJWT, async (req, res) => {
 			const decoded = req.decoded;
 			const uid = req.params.uid;
@@ -286,24 +395,12 @@ const run = async () => {
 			const orderedProduct = await productCollection.findOne(
 				productQuery
 			);
-
-			const updatedDoc = {
-				$set: {
-					promote: false,
-					status: 'sold',
-				},
-			};
-			const productResult = await productCollection.updateOne(
-				productQuery,
-				updatedDoc,
-				option
-			);
 		});
 
 		app.delete(
 			'/product-delete/:uid',
 			verifyJWT,
-			verifyAdmin,
+			verifySeller,
 			async (req, res) => {
 				const decoded = req.decoded;
 				const uid = req.params.uid;
@@ -363,6 +460,66 @@ const run = async () => {
 			const query = {};
 			const blogs = await blogCollection.find(query).toArray();
 			res.send(blogs);
+		});
+
+		//Create Payment Intent
+
+		app.post('/create-payment-intent/:uid', verifyJWT, async (req, res) => {
+			const uid = req.params.uid;
+			const decode = req.decoded;
+			const id = req.query.id;
+			if (uid !== decode.uid) {
+				return res
+					.status(401)
+					.send({ message: 'Unauthorized Access', code: 401 });
+			}
+			const query = { _id: ObjectId(id) };
+			const product = await productCollection.findOne(query);
+			const price = product?.resell_price * 100;
+			const paymentIntent = await stripe.paymentIntents.create({
+				amount: price,
+				currency: 'usd',
+				payment_method_types: ['card'],
+			});
+			res.send({ clientSecret: paymentIntent.client_secret });
+		});
+
+		app.post('/payments/:uid', verifyJWT, async (req, res) => {
+			const uid = req.params.uid;
+			const decode = req.decoded;
+			if (uid !== decode.uid) {
+				return res
+					.status(401)
+					.send({ message: 'Unauthorized Access', code: 401 });
+			}
+			const payment = req.body;
+			const result = await paymentCollection.insertOne(payment);
+			res.send(result);
+			const orderQuery = { _id: ObjectId(payment.orderId) };
+			const option = { upsert: true };
+			const orderUpdatedDoc = {
+				$set: {
+					order_status: true,
+				},
+			};
+			const orderResult = await orderCollection.updateOne(
+				orderQuery,
+				orderUpdatedDoc,
+				option
+			);
+
+			const productQuery = { _id: ObjectId(payment.product_id) };
+			const productUpdatedDoc = {
+				$set: {
+					order_status: true,
+					promote: false
+				},
+			};
+			const productResult = await productCollection.updateOne(
+				productQuery,
+				productUpdatedDoc,
+				option
+			);
 		});
 	} finally {
 	}
